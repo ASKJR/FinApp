7f282137bc0d8858e79351b27d6b6e3eeb06ac28
Alberto Kato <albertokatojr@gmail.com>
2024-09-21 14:11:15 -0300

    activity for create financial statement.

--- app/src/main/java/com/albertokato/finapp/CreateFinancialEntryActivity.kt
+++ app/src/main/java/com/albertokato/finapp/CreateFinancialEntryActivity.kt
@@ -1,31 +1,20 @@
 package com.albertokato.finapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class CreateFinancialEntryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_create_financial_entry)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
-
-    fun close(view: View) {
-        finish()
-    }
-
-    fun financialEntryActivity(view: View) {
-        val intent = Intent(this, CreateFinancialEntryActivity::class.java)
-        startActivity(intent)
     }
 }
